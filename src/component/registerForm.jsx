dba4a40b6824b863fbe45d3b4c96a139b851564f
foziahussaini <fauziakhudadad@gmail.com>
2025-12-18 16:12:20 +0430

    Apply local changes after rebase

--- src/component/registerForm.jsx
+++ src/component/registerForm.jsx
@@ -6,14 +6,22 @@ function registerForm() {
     const [email, setEmail]=useState("");
     const [password, setPassword]=useState("");
 
-    return(
-        <>
-        <input value={FirstName} onChange={(e) => setFirstName(e.target.value)}/>
-        <input value={lastName} onChange={(e) => setLastName(e.target.value)}/>
-        <input value={email} onChange={(e) => setEmail(e.target.value)}/>
-        <input value={password} onChange={(e) => setPassword(e.target.value)}/>
-        </>
-    )
-}
+    import { useState } from "react";
 
-export default registerForm;
+    function RegisterForm() {
+        const [firstName, setFirstName] = useState("");
+        const [lastName, setLastName] = useState("");
+        const [email, setEmail] = useState("");
+        const [password, setPassword] = useState("");
+
+        return (
+            <>
+                <input value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                <input value={lastName} onChange={(e) => setLastName(e.target.value)} />
+                <input value={email} onChange={(e) => setEmail(e.target.value)} />
+                <input value={password} onChange={(e) => setPassword(e.target.value)} />
+            </>
+        );
+    }
+
+    export default RegisterForm;
